64b5616c0e46db221e5387721a0c60d894ad46a7
sw90lee <sw90lee@naver.com>
2022-05-05 12:33:25 +0900

    middleware Token Validation 작성

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,32 @@
 package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/swlee/Ecommerce-sw/tokens"
+	"net/http"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Header에 Token 가져오기
+		ClientToken := c.Request.Header.Get("token")
+		// Token 유무 확인
+		if ClientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": "No authrization Header provied"})
+			c.Abort()
+			return
+		}
+		// Header Token 검증
+		claims, err := tokens.ValidateToken(ClientToken)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+		0
+	}
+}
